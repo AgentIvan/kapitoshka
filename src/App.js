dc09c1db6b0f0084dd185c15fa3b5872b3050193
AgentIvan <AgentIvan@gmail.com>
2021-03-21 19:21:56 +0100

    Add work with firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
-// import { useState } from 'react';
+import firebase from 'firebase';
+import { useState, useEffect } from 'react';
 import MenuHeader from './components/MenuHeader';
 import Footer from './components/Footer';
 import HomePage from './routes/HomePage';
@@ -10,9 +11,103 @@ import ContactPage from './routes/ContactPage';
 import cn from 'classnames';
 import s from './styles.module.css';
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDinTWH8s3GZDiUFV8yaZR-9AN0-3xAjw4",
+  authDomain: "kapitoshka-game.firebaseapp.com",
+  databaseURL: "https://kapitoshka-game-default-rtdb.firebaseio.com",
+  projectId: "kapitoshka-game",
+  storageBucket: "kapitoshka-game.appspot.com",
+  messagingSenderId: "307831794065",
+  appId: "1:307831794065:web:58673c63be65dfb0241230"
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
 const App = () => {
   const location = useLocation();
   const isHomePage = location.pathname === "/" || location.pathname === "/home";
+  const [pokemons, setPokemons] = useState({});
+
+  useEffect(() => {
+    database.ref('pokemons').once('value', (snapshot) => {
+      const values = snapshot.val();
+      console.log('####: App useEffect', values);
+      setPokemons(values);
+    });
+  },[]);
+  const handleSetPokemons = (id) => {
+    console.log('####: App handleSetPokemons id', id);
+    
+    setPokemons(prevState => {
+      const result = Object.entries(prevState).reduce((acc, item) => {
+        // const key = item[0];
+        const pokemon = {...item[1]};
+          if (pokemon.id === id)
+              pokemon.active = true;
+  
+          acc[item[0]] = pokemon;
+          return acc;
+      }, []);
+      console.log('####: App handleSetPokemons result', result);
+
+      // database.ref('pokemons').update('value', pokemons);      
+      return result;
+    });
+  };
+
+  const handleWriteDB = (keyId, active) => {
+    database.ref('pokemons/' + keyId).update({active: !active}, (error) => {
+      if (error) {
+        console.log('####: App error', error);
+      } else {
+        console.log('####: App handleWriteDB Data saved successfully!');
+        setPokemons(prevState => {
+          const result = Object.entries(prevState).reduce((acc, item) => {
+            const key = item[0];
+            const pokemon = {...item[1]};
+              if (keyId === key)
+                  pokemon.active = !pokemon.active;
+      
+              acc[item[0]] = pokemon;
+              return acc;
+          }, []);
+          console.log('####: App handleWriteDB result', result);
+    
+          // database.ref('pokemons').update('value', pokemons);      
+          return result;
+        });
+      }
+    });
+  };
+
+  function handleAddNewPokemon(pokemon) {
+    // A post entry.
+    var newPokemon = {
+      ...pokemon,
+      id: Math.floor(Math.random()*100),
+    };
+  
+    // Get a key for a new Post.
+    const newKey = database.ref().child('pokemons').push().key;
+  
+    // Write the new pokemon's data in the pokemons list.  
+    database.ref('pokemons/' + newKey).set(newPokemon, (error) => {
+      if (error) {
+        console.log('####: App error', error);
+      } else {
+        console.log('####: App handleAddNewPokemon Data saved successfully!');
+        setPokemons(prevState => {
+          const result = { ...prevState, [newKey]:newPokemon };
+          console.log('####: App handleAddNewPokemon new pokemons', result);
+          return result;
+        });
+      }
+    });
+  }
 
   return (
     <Switch>
@@ -27,12 +122,19 @@ const App = () => {
               [s.isHomePage]: isHomePage
             })}>
               <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route path="/home" component={HomePage}/>
-                <Route path="/game" component={GamePage}/>
-                <Route path="/about" component={AboutPage}/>
-                <Route path="/contact" component={ContactPage}/>
-                <Route render={() => <Redirect to="/404"/>}/>                
+                <Route exact path="/" component={HomePage} />
+                <Route path="/home" component={HomePage} />
+                <Route path="/game">
+                  <GamePage 
+                    pokemons={pokemons}
+                    setPokemons={handleSetPokemons}
+                    writeDB={handleWriteDB}
+                    onAddNewPokemon={handleAddNewPokemon}
+                  />
+                </Route>
+                <Route path="/about" component={AboutPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route render={() => <Redirect to="/404" />} />                
               </Switch>
             </div>
           <Footer />
--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,43 +1,46 @@
-import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import s from './styles.module.css';
-import POKEMONS from '../../consts'
 import PokemonCard from '../../components/PokemonCard';
 
-const GamePage = () => {
+const GamePage = ({ pokemons, setPokemons, writeDB, onAddNewPokemon }) => {
     const history = useHistory();
     const handleClick = () => {
         console.log('####: <GamePage />', 'app');
         history.push('/');
     };
-    const [pokemons, setPokemons] = useState(POKEMONS);
-    console.log("####: actived: ",pokemons.filter(el => el.active).map(el => el.id));
-    // const [activeCards, setActiveCards] = useState([]);
-    const handleCardClick = (id) => {
-        // setActiveCards(activeCards => ([...activeCards, id]))
-        // setPokemons(pokemons => pokemons.map(el => el.id === id ? {...el, active: true} : el));
-        setPokemons(pokemons => pokemons.map(el => {
-            if (el.id === id) el["active"] = true;
-            return el;
-        }));
+    const entries = pokemons && Object.entries(pokemons);
+    const handleAddNewPokemon = () => {     
+        console.log('####: <GamePage />', 'handleAddNewPokemon');
+        const rand = Math.floor(Math.random()*5);
+        onAddNewPokemon(entries[rand][1]);
+    };
+    // const handleCardClick = (id) => {
+    //     // setPokemons(pokemons => pokemons.map(el => el.id === id ? {...el, active: true} : el));
+    //     setPokemons(id);
+    // };
+    const handleWriteDB = (keyId, active) => {
+        writeDB && writeDB(keyId, active);
     };
     return (
         <div className={s.root}>
             <div className={s.container}>
-                <button onClick={handleClick}>Go Home</button>
-                <div className="flex">
-                    {pokemons.map(item => 
-                    <PokemonCard
-                        key={item.id}
-                        id={item.id}
-                        name={item.name}
-                        img={item.img}
-                        type={item.type}
-                        values={item.values}
-                        isActive={item.active}
-                        // isActive={activeCards.list.includes(item.id)}
-                        onCardClick={handleCardClick}
-                    />
+                <div className={s.buttons}>
+                    <button className={s.button} onClick={handleAddNewPokemon}>Add New</button>
+                    <button className={s.button} onClick={handleClick}>Go Home</button>
+                </div>
+                <div className={s.flex}>
+                    {pokemons && Object.entries(pokemons).map(([key, item]) => 
+                        item.id && <PokemonCard
+                            key={key}
+                            keyId={key}
+                            id={item.id}
+                            name={item.name}
+                            img={item.img}
+                            type={item.type}
+                            values={item.values}
+                            isActive={item.active}
+                            writeDB={handleWriteDB}
+                        />                    
                     )}
                 </div>
             </div>
