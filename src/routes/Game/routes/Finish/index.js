9553c394709e42bdefa2294e39a5e60413e626db
AgentIvan <AgentIvan@gmail.com>
2021-03-26 09:46:01 +0100

    Add no direct URL to /game/finish

--- src/routes/Game/routes/Finish/index.js
+++ src/routes/Game/routes/Finish/index.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import PokemonCard from "../../../../components/PokemonCard";
 import { PokemonContext } from "../../../../context/pokemonContext";
 import { FireBaseContext } from "../../../../context/firebaseContext";
@@ -8,13 +8,25 @@ import cn from 'classnames';
 import s from './styles.module.css';
 
 const FinishPage = () => {
+  const history = useHistory();
+  const match = useRouteMatch('/game/finish');
+  const win = useRouteMatch('/game/finish/win');
+  const loose = useRouteMatch('/game/finish/loose');
+  const draw = useRouteMatch('/game/finish/draw');
   const { player1, player2, clearPokemonContext } = useContext(PokemonContext);
-  const [isSelected, setSelected] = useState(null);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+  const handleSelectPokemon = (pokemon) => {
+    win && setSelectedPokemon(pokemon);
+    pokemon.id === selectedPokemon?.id && setSelectedPokemon(null);
+  };
+
+  if (match.isExact || !player1 || !player2) {
+    history.replace('/game');
+  }
 
   const firebase = useContext(FireBaseContext);
   
-  const history = useHistory();
 
   const handleEndGameClick = () => {
     selectedPokemon && firebase.addPokemon(selectedPokemon);
@@ -24,6 +36,12 @@ const FinishPage = () => {
 
   return (
     <div className={s.root}>
+      <div className={s.title}>
+        {win && <h1>You win</h1>}
+        {loose && <h1>You loose</h1>}
+        {draw && <h1>Draw</h1>}
+        <h2>Your Cards</h2>
+      </div>
       <div className={s.playerOne}>
         {
           Object.values(player1).map((item) => (
@@ -34,6 +52,7 @@ const FinishPage = () => {
               id={item.id}
               type={item.type}
               values={item.values}
+              minimize
               isActive
             />
           ))
@@ -42,9 +61,15 @@ const FinishPage = () => {
       <div className={s.title}>
         <button
           onClick={handleEndGameClick}
+          disabled={win && !selectedPokemon}
         >
           End Game
         </button>
+        {win && <div>
+          <br />
+          <br />
+          <span><h2>Choose one Enemy`s card to add to the collection and click END GAME</h2></span>
+        </div>}
       </div>
       <div className={s.playerOne}>
         {
@@ -52,11 +77,10 @@ const FinishPage = () => {
             <div
               key={item.id}
               className={cn(s.cardBoard, {
-                [s.selected]: isSelected === item.id
+                [s.selected]: selectedPokemon?.id === item.id
               })}
               onClick={() => {
-                setSelected(item.id);
-                setSelectedPokemon(item);
+                handleSelectPokemon(item);
               }}
             >
             <PokemonCard
@@ -66,6 +90,7 @@ const FinishPage = () => {
               id={item.id}
               type={item.type}
               values={item.values}
+              minimize
               isActive
             />
             </div>
